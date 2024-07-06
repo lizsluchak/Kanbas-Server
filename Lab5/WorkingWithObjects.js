498b76275cae17cfa35374a3db7a1e9cc71c1433
sluchak <sluchak.e@northeastern.edu>
2024-07-06 18:29:25 -0400

    Create a route that responds with the module object, mapped to /lab5/module

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -7,15 +7,35 @@ const assignment = {
     id: 1, title: "NodeJS Assignment",
     description: "Create a NodeJS server with ExpressJS",
     due: "2021-10-10", completed: false, score: 0,
-  };
-  export default function WorkingWithObjects(app) {
+};
+
+const module = {
+    id: "1", 
+    name: "NodeJS Module",
+    description: "Test module object to practice with client",
+    course: "webdev"
+};
+
+
+export default function WorkingWithObjects(app) {
     app.get("/lab5/assignment", (req, res) => {
-      res.json(assignment); //use .json instead of .send i fyou know the response is formatted as JSON
+        res.json(assignment); //use .json instead of .send i fyou know the response is formatted as JSON
     });
 
     app.get("/lab5/assignment/title", (req, res) => {
         res.json(assignment.title);
-      });
-    
-  };
-  
+    });
+
+    app.get("/lab5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params; //object in server
+        assignment.title = newTitle; //persists as long as server is running
+        res.json(assignment);
+    });
+
+    //route that response with module object, mapped to /lab5/module
+    app.get("/lab5/module", (req, res) => {
+        res.json(module); //use .json instead of .send if you know the response is formatted as JSON
+    });
+
+
+};
