30f5d56b98d1f0c9f4386bf0d05e0102ebcd755c
sluchak <sluchak.e@northeastern.edu>
2024-07-07 10:10:08 -0400

    3.4.4. create new data in a server

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -21,9 +21,30 @@ export default function WorkingWithArrays(app) {
             res.json(completedTodos);
             return;
         }
-
         res.json(todos);
     });
+
+    /** 
+     * route that creates a new item in the array and responds with the array now containing new item;
+     * implemented before the /lab5/todos/:id route, otherwise the :id path param would interpret the
+     * "create" in /lab5/todos/create as an ID
+     * 
+     * Note: new todo creates default values including a unique identifier field, id, based on timestamp. 
+     * Eventually primary keys will be handled by a database later in the course. 
+     * 
+     * Response = entire todos array, which is convenient for us now, but more common implementation
+     * would be to respond with newTodo
+     */
+    app.get("/lab5/todos/create", (req, res) => {
+        const newTodo = {
+          id: new Date().getTime(),
+          title: "New Task",
+          completed: false,
+        };
+        todos.push(newTodo);
+        res.json(todos);
+      });
+    
 
     /** route that parses id passed as path param,  finds item, responds with item */
     app.get("/lab5/todos/:id", (req, res) => {
