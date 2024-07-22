575e4d642eb16879d9ad36964e7cd0f6e343d841
sluchak <sluchak.e@northeastern.edu>
2024-07-22 18:27:47 -0400

    search by id

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -26,5 +26,12 @@ export default function UserRoutes(app) {
     res.json(users);
   };
   app.get("/api/users", findAllUsers);
+
+
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
+  app.get("/api/users/:userId", findUserById);
   
 }
