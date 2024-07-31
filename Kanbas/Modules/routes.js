61d868d4f06023a9dad6ca0abe17b83e5988a02f
sluchak <sluchak.e@northeastern.edu>
2024-07-30 22:27:48 -0400

    document created in mongo populates in kanbas

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,4 +1,4 @@
-import Database from "../Database/index.js"
+// import Database from "../Database/index.js"
 import * as dao from "./dao.js";
 
  /**
@@ -13,63 +13,62 @@ import * as dao from "./dao.js";
 
     const findAllModulesROUTE = async (req, res) => {
       const {courseId} = req.params; 
-      console.log(courseId); 
       const modules = await dao.findAllModulesDAO(courseId);
       console.log(modules);
       res.json(modules);
     };
     app.get("/api/modules/:courseId", findAllModulesROUTE);
 
-
+  }
 
     
-//     app.get("/api/courses/:cid/modules", (req, res) => {
-//       const { cid } = req.params;
-//       const modules = Database.modules.filter((m) => m.course === cid);
-//       res.json(modules);
-//     }
-// );
+// //     app.get("/api/courses/:cid/modules", (req, res) => {
+// //       const { cid } = req.params;
+// //       const modules = Database.modules.filter((m) => m.course === cid);
+// //       res.json(modules);
+// //     }
+// // );
 
-/**
- * C: Create Operation for Modules
- */
-app.post("/api/courses/:cid/modules", (req, res) => {
-    const { cid } = req.params;
-    const newModule = {
-      ...req.body,
-      course: cid,
-      _id: new Date().getTime().toString(),
-    };
-    Database.modules.push(newModule);
-    res.send(newModule);
-  });
+// /**
+//  * C: Create Operation for Modules
+//  */
+// app.post("/api/courses/:cid/modules", (req, res) => {
+//     const { cid } = req.params;
+//     const newModule = {
+//       ...req.body,
+//       course: cid,
+//       _id: new Date().getTime().toString(),
+//     };
+//     Database.modules.push(newModule);
+//     res.send(newModule);
+//   });
 
-/**
- * U: Update Operation for Modules
- */
-  app.put("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    const moduleIndex = Database.modules.findIndex(
-      (m) => m._id === mid);
-    Database.modules[moduleIndex] = {
-      ...Database.modules[moduleIndex],
-      ...req.body
-    };
-    res.sendStatus(204);
-  });
-
-
-  /**
-   * D: DELETE Operation for Modules
-   * 
-   * Uses embdedded module ID
-   */
-  app.delete("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    Database.modules = Database.modules.filter((m) => m._id !== mid);
-    res.sendStatus(200);
-  });
+// /**
+//  * U: Update Operation for Modules
+//  */
+//   app.put("/api/modules/:mid", (req, res) => {
+//     const { mid } = req.params;
+//     const moduleIndex = Database.modules.findIndex(
+//       (m) => m._id === mid);
+//     Database.modules[moduleIndex] = {
+//       ...Database.modules[moduleIndex],
+//       ...req.body
+//     };
+//     res.sendStatus(204);
+//   });
 
 
+//   /**
+//    * D: DELETE Operation for Modules
+//    * 
+//    * Uses embdedded module ID
+//    */
+//   app.delete("/api/modules/:mid", (req, res) => {
+//     const { mid } = req.params;
+//     Database.modules = Database.modules.filter((m) => m._id !== mid);
+//     res.sendStatus(200);
+//   });
 
-}
+
+
+// }
